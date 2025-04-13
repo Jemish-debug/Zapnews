1e74ba318f518b60d82fdffe35b261001b099b0c
Jemish-debug <jemishkoladiya@gmail.com>
2025-04-13 14:56:30 +0530

    Sowwy is left now

--- sendEmail.py
+++ sendEmail.py
@@ -19,19 +19,3 @@ def send_email(sender_email, sender_password, receiver_email, subject, body):
         print("✅ Email sent successfully!")
     except Exception as e:
         print(f"❌ Failed to send email: {e}")
-
-# send_email(
-#     sender_email="koladiajemish@gmail.com",
-#     sender_password="cufc scjo owzx brxv",
-#     receiver_email="manaspathak100@gmail.com",
-#     subject="Chicken Pox",
-#     body="Kesa hai?"
-# )
-
-# send_email(
-#     sender_email="koladiajemish@gmail.com",
-#     sender_password="cufc scjo owzx brxv",
-#     receiver_email="vanshpatelvsp@gmail.com",
-#     subject="CRAZY",
-#     body="Automation on it's peak"
-# )
